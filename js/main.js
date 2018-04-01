b3875ad83fcc84c82cf0a886003fa82100e7db19
Anatoly <AnatoliySokolov1981@gmail.com>
2018-04-01 13:30:04 +0300

    Замечания
    Б17. Добавлена ссылка на сервис карт
    Д14. Скрипты собраны в отдельном js
    Д16. Реализовано открыти модального окна с формой
    Д.17 Добавлена анимация формы
    Д.20 Добавлена ссылка на стр write-us.html

---
+++ js/main.js
@@ -0,0 +1,80 @@
+var feedback = document.querySelector(".feedback-js");
+var feedbackPopup = document.querySelector(".modal-write-us");
+var feedbackClose = document.querySelector(".modal-write-us .modal-close");
+var fullname = document.querySelector("[name=fullname]");
+var form = feedbackPopup.querySelector("form");
+var email = feedbackPopup.querySelector("[name=email]");
+
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("fullname");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+feedback.addEventListener("click", function (event) {
+  event.preventDefault();
+  feedbackPopup.classList.add("modal-show");
+  if (storage) {
+    fullname.value = storage;
+    email.focus();
+  } else {
+    fullname.focus();
+  }
+});
+
+feedbackClose.addEventListener("click", function (event) {
+  event.preventDefault();
+  feedbackPopup.classList.remove("modal-show");
+  feedbackPopup.classList.remove("modal-error");
+});
+
+form.addEventListener("submit", function (event) {
+  if (!fullname.value || !email.value) {
+    event.preventDefault();
+    feedbackPopup.classList.remove("modal-error");
+    feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
+    feedbackPopup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("fullname", fullname.value);
+    }
+  }
+});
+
+window.addEventListener("keydown", function (event) {
+  if (event.keyCode === 27) {
+    event.preventDefault();
+    if (feedbackPopup.classList.contains("modal-show")) {
+      feedbackPopup.classList.remove("modal-show");
+      feedbackPopup.classList.remove("modal-error");
+    }
+  }
+});
+
+//************************************КАРТА*****************************************************
+
+var mapLink = document.querySelector(".contacts-button-map");
+var mapPopup = document.querySelector(".modal-map");
+var mapClose = mapPopup.querySelector(".modal-close");
+
+mapLink.addEventListener("click", function (event) {
+  event.preventDefault();
+  mapPopup.classList.add("modal-show");
+});
+
+mapClose.addEventListener("click", function (event) {
+  event.preventDefault();
+  mapPopup.classList.remove("modal-show");
+});
+
+window.addEventListener("keydown", function (event) {
+  event.preventDefault();
+  if (event.keyCode === 27) {
+    if (mapPopup.classList.contains("modal-show")) {
+      mapPopup.classList.remove("modal-show");
+    }
+  }
+});
