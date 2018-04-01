275beb534826e1429433efa4a5ac5dfc1a6c031e
Anatoly <AnatoliySokolov1981@gmail.com>
2018-04-01 16:14:58 +0300

    Д11 Отрисованно невалидное состояние поля почты в форме обратной связи

--- js/main.js
+++ js/main.js
@@ -56,25 +56,25 @@ window.addEventListener("keydown", function (event) {
 
 //************************************КАРТА*****************************************************
 
-var mapLink = document.querySelector(".contacts-button-map");
-var mapPopup = document.querySelector(".modal-map");
-var mapClose = mapPopup.querySelector(".modal-close");
-
-mapLink.addEventListener("click", function (event) {
-  event.preventDefault();
-  mapPopup.classList.add("modal-show");
-});
-
-mapClose.addEventListener("click", function (event) {
-  event.preventDefault();
-  mapPopup.classList.remove("modal-show");
-});
-
-window.addEventListener("keydown", function (event) {
-  event.preventDefault();
-  if (event.keyCode === 27) {
-    if (mapPopup.classList.contains("modal-show")) {
-      mapPopup.classList.remove("modal-show");
-    }
-  }
-});
+//var mapLink = document.querySelector(".contacts-button-map");
+//var mapPopup = document.querySelector(".modal-map");
+//var mapClose = mapPopup.querySelector(".modal-close");
+//
+//mapLink.addEventListener("click", function (event) {
+//  event.preventDefault();
+//  mapPopup.classList.add("modal-show");
+//});
+//
+//mapClose.addEventListener("click", function (event) {
+//  event.preventDefault();
+//  mapPopup.classList.remove("modal-show");
+//});
+//
+//window.addEventListener("keydown", function (event) {
+//  event.preventDefault();
+//  if (event.keyCode === 27) {
+//    if (mapPopup.classList.contains("modal-show")) {
+//      mapPopup.classList.remove("modal-show");
+//    }
+//  }
+//});
